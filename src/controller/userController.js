f8bdb92bca5a30501ed00a18a9189ea4913031d7
ShibeshChaudhary <chyshiwesh123@gmail.com>
2025-12-02 07:05:25 +0545

    ss

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -23,3 +23,19 @@ exports.getUserById = async (req, res) => {
     res.status(500).json({ msg: "Server error" });
   }
 };
+exports.getMyProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user)
+      return res.status(404).json({ msg: "User not found" });
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    console.error("Error getting profile:", err);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,9 +4,11 @@ const router=express.Router();
 const usercontroller=require("../controller/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const roleMiddleware = require("../middlewares/roleMiddleware");
+const { route } = require("./auth");
 
 router.get("/users",authMiddleware,roleMiddleware("admin"),usercontroller.getAllUsers);
 router.get("/:id",authMiddleware,usercontroller.getUserById);
+route.get("/me",authMiddleware,usercontroller.getMyProfile);
 
 
 module.exports = router;
