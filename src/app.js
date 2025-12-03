a79b6b4e1a8e765baa47c804030f50c6e75dc8eb
ShibeshChaudhary <chyshiwesh123@gmail.com>
2025-12-03 06:50:35 +0545

    1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors=require('cors');
 const authRoutes = require("./routes/auth");
-
 const postRoutes=require("./routes/post");
 const userRoutes = require("./routes/user");
 const app = express();
